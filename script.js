60a657d96e5b993bac29dc0d385db7e2b5ec5f97
Samuel <samueluebari7@gmail.com>
2024-04-11 22:00:57 -0700

    index.html, script.js, .gitignore,

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ const input = document.querySelector('.input-form')
 const searchInput= document.querySelector('.search-input')
 
 let filteredProduct = [...products]
-// console.log(filteredProduct);
-
 function display(){
 
     if(filteredProduct.length < 1){
         productsContainer.innerHTML=`<h6>Sorry, no match found</h6>`
         return
     }
-    
-
     productsContainer.innerHTML = filteredProduct.map((product)=>{
         const {id,image,title,price} = product
-        // console.log(id,image,title,price);
         return `<article class="product" data-id="${id}">
         <img
           src="${image}"
@@ -24,7 +19,7 @@ function display(){
         />
         <footer>
           <h5 class="product-name">${title}</h5>
-          <span class="product-price">${price}</span>
+          <span class="product-price">$${price}</span>
         </footer>
       </article>`
     }).join('')
@@ -43,42 +38,66 @@ input.addEventListener('keyup',()=>{
 
 // Filter Buttons
 
-const companiesDOM = document.querySelector('.companies');
+// const companiesDOM = document.querySelector('.companies');
 
-const displayButtons = () => {
-  const buttons = [
-    'all',
-    ...new Set(products.map((product) => product.company)),
-  ];
+// const displayButtons = () => {
+//   const buttons = [
+//     'all',
+//     ...new Set(products.map((product) => product.company)),
+//   ];
   // console.log(buttons);
-  companiesDOM.innerHTML = buttons
-    .map((company) => {
-      return `<button class='company-btn' data-id="${company}">${company}</button>`;
-    })
-    .join('');
-};
+  // console.log(buttons);
+//   companiesDOM.innerHTML = buttons
+//     .map((company) => {
+//       return `<button class='company-btn' data-id="${company}">${company}</button>`;
+//     })
+//     .join('');
+// };
 
-displayButtons();
+// displayButtons();
 
-companiesDOM.addEventListener('click', (e) => {
-  const el = e.target;
-  if (el.classList.contains('company-btn')) {
-    if (el.dataset.id === 'all') {
+// companiesDOM.addEventListener('click', (e) => {
+//   const el = e.target;
+//   if (el.classList.contains('company-btn')) {
+//     if (el.dataset.id === 'all') {
+//       filteredProduct = [...products];
+//     } else {
+//       filteredProduct = products.filter((product) => {
+//         return product.company === el.dataset.id;
+//       });
+//     }
+//     searchInput.value = '';
+//     display();
+//     console.log(el);
+//   }
+// });
+
+
+
+
+// Filter Button
+const companiesDOM = document.querySelector('.companies');
+function show (){
+  let buttons = ['all',...new Set(products.map(product=> product.company))]
+    companiesDOM.innerHTML = buttons.map((company)=>{
+    return `<button class ='company-btn' data-id=${company}>${company}</button>`
+  }).join('')
+
+}
+show()
+companiesDOM.addEventListener('click',(e)=>{
+  const el = e.target
+  if(el.classList.contains('company-btn')){
+    if(el.dataset.id === 'all'){
       filteredProduct = [...products];
-    } else {
-      filteredProduct = products.filter((product) => {
-        return product.company === el.dataset.id;
-      });
+    }else{
+      filteredProduct = products.filter((product)=>{
+        return product.company === el.dataset.id
+      })
     }
-    searchInput.value = '';
-    display();
   }
-});
+  searchInput.value = '';
+  display()
+})
 
-
-// console.log();
-// let num = new Set(1,2,3,4,5)
-// console.log(num);
-const num = [1,2,3,4,3,4,6,8,5]
-let uniqValue = new Set(num)
-console.log(num,uniqValue);
+// Map iterate over an array an return an array base on the size of the original array 
